d713409457e6579e36d1b72ba6536121f59b36a3
Arun1213 <58766777+Arun1213@users.noreply.github.com>
2019-12-11 15:01:23 +0530

    help

---
+++ masterphp.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+-->
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>Arun tecnology</title>
+
+<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+<body>
+<div id="menu-wrapper">
+	<div id="menu">
+		<ul>
+			<li class="current_page_item"><a href="#">Homepage</a></li>
+			
+			<li><a href="#">Photos</a></li>
+			<li><a href="#">About</a></li>
+			<li><a href="#">Contact</a></li>
+		</ul>
+	</div>
+-->
+
+
+<div id="header-wrapper">
+	<div id="header">
+		<div id="logo">
+                    <h1><a href="#">Arun tecnology</a></h1>
+			
+		</div>
+	</div>
+</div>
+ --> 
+
+
+<div id="wrapper"> 
+	<!-- end #header -->
+	<div id="page-bgtop"></div>
+	<div id="page">
+		<div><img src="arun image.jpg" width="920" height="300" alt="" /></div>
+		<div id="content">
+			<div class="post">
+				<h2 class="title"><a href="#">Welcome  to  Arun  Tecnology </a></h2>
+				<p class="meta"><span class="date">Nov 27, 2019</span><span class="posted">Posted by <a href="#">Arun</a></span></p>
+				<div style="clear: both;">&nbsp;</div>
+				<div class="entry">
+                                    <p>ARUN Technology has been dedicated to the design, development and manufacture of optical emission spectrometers since launching the world’s first portable CCD based metals analyser in the 1980’s.
+
+Continuous and extensive investment in research and development is ensuring that ARUN Technology products remain at the forefront of metals analysis.
+
+Today, ARUN Technology and its worldwide network of Channel Partners offer a range of stationary desktop spectrometers. These include the ARTUS 10, ARTUS 8 and MERLIN 4.
+
+Our spectrometers are designed to perform quick and reliable measurements. We offer a wide portfolio of calibrations to choose from. If you require something special: it may be possible to create tailor-made calibrations by our Application Development team.
+                                        
+</p>	
+				</div>
+			</div>
+			<div class="post">
+				<h2 class="title"><a href="#">Service Training</a></h2>
+				<p class="meta"><span class="date">Nov27, 2019</span><span class="posted">Posted by <a href="#">Arun</a></span></p>
+				<div style="clear: both;">&nbsp;</div>
+				<div class="entry">
+                                    <p>
+                                        Bi-yearly training is offered at ARUN Technology at our Centre for Excellence located in West Sussex, United Kingdom. These training programs help you get the most from your time, while out in the field. Topics range from theory, sample handling, calibrations, applications, maintenance and troubleshooting.
+                                    </p>
+					<p class="links"><a href="#">Read More</a>&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;<a href="#">Comments</a></p>
+				</div>
+			</div>
+			<div style="clear: both;">&nbsp;</div>
+		</div>
+		<!-- end #content -->
+		<div id="sidebar">
+			<ul>
+				<li>
+					<h2>SearCh Here:</h2>
+					<div id="search" >
+						<form method="get" action="#">
+							<div>
+								<input type="text" name="s" id="search-text" value="" />
+								<input type="submit" id="search-submit" value="" />
+							</div>
+						</form>
+					</div>
+					<div style="clear: both;">&nbsp;</div>
+				</li>
+				
+				<li>
+					<h2>Choose your Instrument</h2>
+					<ul>
+						<li><a href="#">ARTUS 10</a></li>
+						<li><a href="#">MERLIN 4</a></li>
+						<li><a href="#">POLYSPEK JUNIOR</a></li>
+						<li><a href="#">ARTUS 8</a></li>
+						
+					</ul>
+				</li>
+				
+			</ul>
+		</div>
+		<!-- end #sidebar -->
+		<div style="clear: both;">&nbsp;</div>
+	</div>
+	<div id="page-bgbtm"></div>
+	<!-- end #page --> 
+</div>
+<?php
+include'./themepart/footer.php';
+?>
+</body>
+</html>
+
